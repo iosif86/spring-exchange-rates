5ee35393c5c06c37e3c6b88806ad5093e1ebbc42
Iosif <iosif.balog@c4media.com>
2025-03-24 18:52:29 +0200

    added project structure

--- README.md
+++
@@ -1,1 +0,0 @@
-# spring-exchange-rates
---
+++ src/test/java/bv/group/exchangerates/ExchangeRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package bv.group.exchangerates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
